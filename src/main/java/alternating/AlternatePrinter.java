f809d87fa15e6fceb602b8a5a6f72ccacbcff3b0
Oleh <basarab.oleh@lll.kpi.ua>
2024-02-20 10:35:45 +0200

    reworked AsyncCharPrinter (number of lines to print - 50, number of symbols in line - 100)

--- src/main/java/alternating/AlternatePrinter.java
+++ src/main/java/alternating/AlternatePrinter.java
@@ -6,8 +6,8 @@ public class AlternatePrinter {
         Thread vbarThread = new Thread(new SyncCharPrinter('|', sync));
         Thread hbarThread = new Thread(new SyncCharPrinter('-', sync));
 
-//        Thread vbarThread = new Thread(new AsyncCharPrinter('|'));
-//        Thread hbarThread = new Thread(new AsyncCharPrinter('-'));
+        //Thread vbarThread = new Thread(new AsyncCharPrinter('|'));
+        //Thread hbarThread = new Thread(new AsyncCharPrinter('-'));
         vbarThread.start();
         hbarThread.start();
     }
@@ -22,8 +22,8 @@ class AsyncCharPrinter implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 50; j++) {
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; j < 100; j++) {
                 System.out.print(this.character);
             }
             System.out.println();
